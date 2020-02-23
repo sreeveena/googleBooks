341ea5ddd9425120377d8a18d6bd7902375f7667
sreeveena <sreeveena21@gmail.com>
2020-02-22 21:18:57 -0800

    readme

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
+// if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-}
+// }
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googleBooks");
 
